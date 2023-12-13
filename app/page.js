1e2f8ed1db944bfd0baa6d8c9d14e42a3fcdbf00
thitipongr <thitipong.r.psn@gmail.com>
2023-12-13 22:50:24 +0700

    initial tailwind to navbar

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Navbar from "./components/navigation/page";
+
+export default function Home() {
+  // มีหน้าสำหรับโพสต์ข้อความ ปุ่มกดไลค์ แสดงความคิดเห็น แชร์โพสต์
+  // หน้าโปรไฟล์ของผู้ใช้
+  // หน้าแชท
+  
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
